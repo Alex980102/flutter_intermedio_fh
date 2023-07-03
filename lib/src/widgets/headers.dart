9e5fd239c25d13a2bf3753c76fd0ee0fbd0afec9
Alex980102 <alex.980102@gmail.com>
2023-07-02 22:16:23 -0600

    Add curvos

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -142,3 +142,99 @@ class _HeaderPicoPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderCurvo extends StatelessWidget {
+  const HeaderCurvo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // Propiedades
+    paint.color = const Color(0xff615AAB);
+    paint.style = PaintingStyle
+        .fill; // .stoke -> serían solo los bordes y el .fill -> sería rellenar todo el espacio
+    paint.strokeWidth = 2;
+
+    final path = Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.25);
+
+    path.quadraticBezierTo(
+      size.width / 2,
+      size.height * 0.15,
+      size.width,
+      size.height * 0.25,
+    );
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderDosCurvas extends StatelessWidget {
+  const HeaderDosCurvas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDosCurvasPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDosCurvasPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // Propiedades
+    paint.color = const Color(0xff615AAB);
+    paint.style = PaintingStyle
+        .fill; // .stoke -> serían solo los bordes y el .fill -> sería rellenar todo el espacio
+    paint.strokeWidth = 2;
+
+    final path = Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.2);
+
+    path.quadraticBezierTo(
+        size.width / 4, size.height * 0.26, size.width / 2, size.height * 0.2);
+
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.14, size.width, size.height * 0.2);
+
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
